505c099c1107eb048b7cd5fb9d0b41ab46ec5ade
fortyguide <stefano1994>
2021-12-22 16:35:34 +0100

    22/12/2021 Interfacce in azione

--- src/interfacce_e_classi_astratte/Interfaccia.kt
+++ src/interfacce_e_classi_astratte/Interfaccia.kt
@@ -1,5 +1,30 @@
 package interfacce_e_classi_astratte
 
-fun main() {
+interface A{
+    fun miaFunzione(){
+        println("Sono la funzione dell'interfaccia A")
+    }
+}
 
+interface B{
+
+    fun miaFunzione(){
+        println("Sono la funzione dell'interfaccia B")
+    }
+}
+
+class X: A,B{
+    override fun miaFunzione() {
+        super<A>.miaFunzione()//super in questo caso serve per indicare a quale interfaccia ci riferiamo
+        super<B>.miaFunzione()
+
+        //blocco di codice...
+    }
+
+}
+
+fun main() {
+    val x = X()
+    x.miaFunzione()//Sono la funzione dell'interfaccia A
+                   //Sono la funzione dell'interfaccia B
 }
